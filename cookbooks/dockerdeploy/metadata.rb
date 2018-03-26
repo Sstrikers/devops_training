15cfa746f0de095ac5527d117eb38e918a828183
Sergey Tsurankov <sstrikers@gmail.com>
2018-03-26 20:06:46 +0000

    Updated. Current verison is 1.0.20

--- cookbooks/dockerdeploy/metadata.rb
+++ cookbooks/dockerdeploy/metadata.rb
@@ -5,7 +5,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures dockerdeploy'
 long_description 'Installs/Configures dockerdeploy'
-version '1.0.18'
+version '1.0.20'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
