bf75494a625ea585e23fdb12b7366614f94335e2
Sergey Tsurankov <sstrikers@gmail.com>
2018-03-26 22:19:01 +0300

    Update default.rb

--- cookbooks/dockerdeploy/attributes/default.rb
+++ cookbooks/dockerdeploy/attributes/default.rb
@@ -1,4 +1,4 @@
 #
 #Mon Mar 26 19:12:01 UTC 2018
-node.default['dockerdeploy']['registry_url']='192.168.56.10\:5000'
+node.default['dockerdeploy']['registry_url']='192.168.56.10:5000'
 node.default['dockerdeploy']['imageVersion']='1.0.18'
