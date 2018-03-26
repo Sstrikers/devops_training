5c066faaf773ae5757bfb91d4386a340e538907f
Sergey Tsurankov <sstrikers@gmail.com>
2018-03-26 20:39:25 +0000

    Updated. Current verison is 1.0.18

--- cookbooks/dockerdeploy/attributes/default.rb
+++ cookbooks/dockerdeploy/attributes/default.rb
@@ -1,5 +1,5 @@
 #
-#Mon Mar 26 20:06:33 UTC 2018
+#Mon Mar 26 20:39:14 UTC 2018
 node.default['dockerdeploy']['registry_port']='5000'
 node.default['dockerdeploy']['registry_url']='192.168.56.10'
-node.default['dockerdeploy']['imageVersion']='1.0.20'
+node.default['dockerdeploy']['imageVersion']='1.0.18'
